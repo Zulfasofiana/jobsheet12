14ca3408909d8b691602f11f0f06ecdc68e3c25d
zuliana21 <zuli25176@gmail.com>
2022-10-05 16:02:54 +0700

    first commit

---
+++ app/src/main/java/com/example/jobsheet12sofi/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.jobsheet12sofi
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button1)
+        val btn2 = findViewById<Button>(R.id.button2)
+
+        btn.setOnClickListener {
+
+            val Intent = Intent(  this, MainActivity2::class.java)
+            startActivity(Intent)
+        }
+        btn2.setOnClickListener {
+
+            val Intent = Intent(  this, MainActivity3::class.java)
+            startActivity(Intent)
+        }
+    }
+
+}
